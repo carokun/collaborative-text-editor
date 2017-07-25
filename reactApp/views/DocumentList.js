f9d3092c0ac86f1ba4637266171f5df2e8042046
teresaliu20 <teresali@usc.edu>
2017-07-24 17:17:49 -0700

    added buttons

--- reactApp/views/DocumentList.js
+++ reactApp/views/DocumentList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import styles from '../assets/stylesheets/documentlist.less';
 
 class DocumentList extends React.Component {
   constructor(props) {
