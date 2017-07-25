33ced154709c25afe644930bfe6e5e73169053b5
caroline <carokun@sas.upenn.edu>
2017-07-25 12:02:50 -0700

    sockets test

--- backend/sockets.js
+++ backend/sockets.js
@@ -2,31 +2,13 @@ module.exports = {
   sockets: function(io) {
     io.on('connection', socket => {
       console.log('connected');
-
-    //   socket.on('document', requestedRoom => {
-    //     socket.room = requestedRoom;
-    //     socket.join(requestedRoom, () => {
-    //       socket.to(requestedRoom).emit('message', {
-    //         username: 'System',
-    //         content: `${socket.username} has joined`
-    //       });
-    //     });
-    //   });
-    //
-    //   socket.on('message', message => {
-    //     if (!socket.room) {
-    //       return socket.emit('errorMessage', 'No rooms joined!');
-    //     }
-    //     socket.to(socket.room).emit('message', {
-    //       username: socket.username,
-    //       content: message
-    //     });
-    //   })
-    //
-    //   socket.on('typing', message => {
-    //     socket.broadcast.emit('typing', message);
-    //   })
-    // });
+      socket.on('documentChange', currentState => {
+          if (!currentState) {
+            return socket.emit('errorMessage', 'No document!');
+          }
+          console.log('curr', currentState);
+          socket.broadcast.emit('documentChange', currentState);
+      });
     })
   }
 }
--- reactApp/components/Editor.js
+++ reactApp/components/Editor.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {RichUtils, Editor, EditorState, convertToRaw, convertFromRaw} from 'draft-js';
 import axios from 'axios';
-
+import io from 'socket.io-client'
 
 const styleMap = {
   'LEFT_ALIGN': {
@@ -23,11 +23,14 @@ class MyEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      socket: io(),
+      socket: io('http://localhost:3000'),
       editorState: EditorState.createEmpty(),
       interval: () => ''
     };
-    this.onChange = (editorState) => this.setState({editorState});
+    this.onChange = (editorState) => {
+      this.setState({editorState})
+      this.state.socket.emit('documentChange', convertToRaw(this.state.editorState.getCurrentContent()))
+    };
   }
 
   _onBoldClick() {
@@ -77,7 +80,9 @@ class MyEditor extends React.Component {
   componentDidMount() {
     this.state.socket.on('connect', () => {
       console.log("connected on the client side");
-      
+      this.state.socket.on('documentChange', (currentContent) => {
+        this.onChange(EditorState.createWithContent(convertFromRaw(currentContent)));
+      })
     })
   }
 
