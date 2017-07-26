5f33d7e1f88d5ece16049ed8903ac7b72c197856
teresaliu20 <teresali@usc.edu>
2017-07-26 09:21:03 -0700

    Fixed algorithm

--- reactApp/views/RevisionHistory.js
+++ reactApp/views/RevisionHistory.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import {Editor, EditorState, convertToRaw, convertFromRaw} from 'draft-js';
+import { Editor, EditorState, convertToRaw, convertFromRaw } from 'draft-js';
 import moment from 'moment';
 
 class RevisionHistory extends React.Component {
@@ -9,17 +9,19 @@ class RevisionHistory extends React.Component {
     this.state = {
       revisionhistory: [],
       editorState: EditorState.createEmpty(),
-      title: ""
+      title: "",
+      currVersion: "",
+      displayedChanges: []
     }
   }
 
   componentWillMount() {
     axios.get('http://localhost:3000/document/' + this.props.match.params.docId)
     .then(resp => {
-      console.log("RESPON", resp.data.revisionhistory);
       this.setState({
         revisionhistory: resp.data.revisionhistory,
-        title: resp.data.title
+        title: resp.data.title,
+        currVersion: resp.data.text
       });
     })
     .catch(err => {
@@ -27,11 +29,51 @@ class RevisionHistory extends React.Component {
     });
   }
 
+  updateChangesDisplay(prevState) {
+    var currVersion = EditorState.createWithContent(convertFromRaw(JSON.parse(this.state.currVersion))).getCurrentContent().getPlainText().split(/\r?\n/);
+    var viewedVersion = prevState.getCurrentContent().getPlainText().split(/\r?\n/);
+
+    currVersion = currVersion.filter(function(entry) { return entry.trim() !== ''; });
+    viewedVersion = viewedVersion.filter(function(entry) { return entry.trim() !== ''; });    
+
+    console.log("CURRENT VERSION: ", currVersion);
+    console.log("EDITOR STATE: ", viewedVersion);
+
+    var changesArray = [];
+
+    let currIndex = 0; // Index to track place in currVersion (current saved version of document)
+    let pastIndex = 0; // Index to track place in viewedVersion (previous version of document)
+
+    while (currIndex < currVersion.length || pastIndex < viewedVersion.length) {
+      console.log("HERE")
+      if (currVersion[currIndex] === viewedVersion[pastIndex]) {
+        changesArray.push(<div className="not-changed-line">{currVersion[currIndex]}</div>)
+        currIndex++;
+        pastIndex++;
+      }
+      else {
+        while (currVersion[currIndex] !== viewedVersion[pastIndex] && currIndex < currVersion.length) {
+          changesArray.push(<div className="added-line">{currVersion[currIndex]}</div>);
+          currIndex++;
+        }
+        while (currVersion[currIndex] !== viewedVersion[pastIndex] && pastIndex < viewedVersion.length) {
+          changesArray.push(<div className="removed-line">{viewedVersion[pastIndex]}</div>);
+          pastIndex++;
+        }
+      }
+    }
+    console.log("DISPLAYED CHANGES: ", changesArray);
+    this.setState({
+      displayedChanges: changesArray
+    })
+  }
+
   modifyEditorDisplay(prevEditorState) {
     const prevState = EditorState.createWithContent(convertFromRaw(JSON.parse(prevEditorState.revision)));
     this.setState({
       editorState: prevState
-    })
+    });
+    this.updateChangesDisplay(prevState);
   }
 
   restore() {
@@ -40,8 +82,11 @@ class RevisionHistory extends React.Component {
       prevState: JSON.stringify(convertToRaw(this.state.editorState.getCurrentContent()))
     })
     .then(resp => {
-      console.log('response', resp);
+      this.setState({
+        currVersion: JSON.stringify(convertToRaw(this.state.editorState.getCurrentContent()))
+      });
     })
+    .catch(err => console.log("ERROR: ", err))
   }
 
   render() {
@@ -55,6 +100,14 @@ class RevisionHistory extends React.Component {
           readOnly={true}
         />
         </div>
+
+        <h3>Changes</h3>
+        <div className="changes-view">
+          {
+            this.state.displayedChanges.map((line) => (line))
+          }
+        </div>
+
         <h3>History</h3>
         <h5>Least Recent</h5>
         <div className="history-list">
